9fd7ed71ce5a00b71fc9b484f8fb2047f862e6dd
ABedychaj <andrzej.bedychaj@karhoo.com>
2020-05-31 19:09:51 +0200

    Mutual information gain parameter added

--- src/Loss/Loss.py
+++ src/Loss/Loss.py
@@ -1,6 +1,5 @@
 import torch
-from torch import ones, multinomial
-from torch.distributions import MultivariateNormal
+from sklearn.metrics import normalized_mutual_info_score
 
 
 class LossFunction:
@@ -11,63 +10,10 @@ class LossFunction:
         raise NotImplementedError
 
 
-class WeightedICALossFunction(LossFunction):
-    def __init__(self, power, number_of_gausses, cuda, z_dim=None):
-        super(WeightedICALossFunction, self).__init__()
-        self.power = power
-        self.number_of_gausses = number_of_gausses
-        self.z_dim = z_dim
+class MutualInformationLoss(LossFunction):
+    def __init__(self, cuda=False):
+        super(MutualInformationLoss, self).__init__()
         self.cuda = cuda
-        self.reduction_type = "mean"
 
-    def random_choice_full(self, input, n_samples):
-        if n_samples * self.number_of_gausses < input.shape[0]:
-            replacement = False
-        else:
-            replacement = True
-        idx = multinomial(ones(input.shape[0]), n_samples * self.number_of_gausses, replacement=replacement)
-        sampled = input[idx].reshape(self.number_of_gausses, n_samples, -1)
-        return torch.mean(sampled, axis=1)
-
-    def loss(self, z, latent_normalization=False):
-        if latent_normalization:
-            x = (z - z.mean(dim=1, keepdim=True)) / z.std(dim=1, keepdim=True)
-        else:
-            x = z
-        dim = self.z_dim if self.z_dim is not None else x.shape[1]
-        scale = (1 / dim) ** self.power
-        sampled_points = self.random_choice_full(x, dim)
-
-        cov_mat = (scale * torch.eye(dim)).repeat(self.number_of_gausses, 1, 1).type(torch.float64)
-        if self.cuda:
-            cov_mat = cov_mat.cuda()
-
-        mvn = MultivariateNormal(loc=sampled_points,
-                                 covariance_matrix=cov_mat)
-
-        weight_vector = torch.exp(mvn.log_prob(x.reshape(-1, 1, dim)))
-
-        sum_of_weights = torch.sum(weight_vector, axis=0)
-        weight_sum = torch.sum(x * weight_vector.T.reshape(self.number_of_gausses, -1, 1), axis=1)
-        weight_mean = weight_sum / sum_of_weights.reshape(-1, 1)
-
-        xm = x - weight_mean.reshape(self.number_of_gausses, 1, -1)
-        wxm = xm * weight_vector.T.reshape(self.number_of_gausses, -1, 1)
-
-        wcov = (wxm.permute(0, 2, 1).matmul(xm)) / sum_of_weights.reshape(-1, 1, 1)
-
-        diag = torch.diagonal(wcov ** 2, dim1=1, dim2=2)
-        diag_pow_plus = diag.reshape(diag.shape[0], diag.shape[1], -1) + diag.reshape(diag.shape[0], -1, diag.shape[1])
-
-        tmp = (2 * wcov ** 2 / diag_pow_plus)
-
-        triu = torch.triu(tmp, diagonal=1)
-        normalize = 2.0 / (dim * (dim - 1))
-        cost = torch.sum(normalize * triu) / self.number_of_gausses
-        return cost
-
-
-def MINE(T_xy, T_x_y):
-    # compute the negative loss (maximise loss == minimise -loss)
-    neg_loss = -(torch.mean(T_xy) - torch.log(torch.mean(torch.exp(T_x_y))))
-    return neg_loss
+    def loss(self, x, y):
+        return 1 - normalized_mutual_info_score(torch.flatten(y), torch.flatten(x))
--- src/TrainingOnMNIST.py
+++ src/TrainingOnMNIST.py
@@ -10,17 +10,21 @@ from torchvision.utils import save_image
 
 from src.Decoders.Decoder import FCDecoder
 from src.Encoders.Encoder import FCEncoder
-from src.Loss.Loss import WeightedICALossFunction
+from src.Loss.Loss import MutualInformationLoss
 
 
-def save_images_from_epoch(imgs, epoch, folder, save_every):
+def save_images_from_epoch(imgs, epoch, folder, save_every, train=True):
     if (epoch + 1) % save_every == 0:
+        if train is True:
+            name = 'train_image_{}.png'
+        else:
+            name = 'test_image_{}.png'
         save_train = imgs.cpu().data.clamp(0, 1)
         save_image(save_train,
-                   os.path.join(os.path.join(folder, 'images'), 'train_image_{}.png'.format(epoch)))
+                   os.path.join(os.path.join(folder, 'images'), name.format(epoch)))
 
 
-def report(epoch_, num_epochs_, start_, end_, loss_, save_every=1, train=True):
+def report(epoch_, num_epochs_, start_, end_, loss_, save_every=2, train=True):
     if epoch_ % save_every == 0:
         if train:
             print("Training...")
@@ -67,10 +71,9 @@ neurons = 256
 num_epochs = 21
 lr = 1E-3
 batch_size = 128
-wica_loss = WeightedICALossFunction(power=0, number_of_gausses=50, cuda=False)
-beta = 10
+beta = 1
 criterion = nn.MSELoss()
-
+MI_loss = MutualInformationLoss()
 train_dataloader = torch.utils.data.DataLoader(datasets.MNIST('../data',
                                                               download=True,
                                                               train=True,
@@ -89,15 +92,15 @@ test_dataloader = torch.utils.data.DataLoader(datasets.MNIST('../data',
                                                                  transforms.Normalize((0.1307,), (0.3081,))
                                                              ])),
                                               batch_size=batch_size,
-                                              shuffle=True)
+                                              shuffle=False)
 
 for _ in range(1):
     for l in latent_dim:
         for h in hidden_dim:
             # hidden_dims=[neurons // (_ + 1) for _ in range(h)]
             hidden_dims_e = [neurons for _ in range(h)]
-            encoder_activations = [nn.Sequential(nn.ReLU(), nn.BatchNorm1d(_)) for _ in hidden_dims_e]
-            encoder_activations.extend([nn.Sequential(nn.ReLU(), nn.BatchNorm1d(l))])
+            encoder_activations = [nn.Sequential(nn.ReLU()) for _ in hidden_dims_e]
+            encoder_activations.extend([nn.Sequential(nn.ReLU())])
 
             encoder = FCEncoder(
                 input_dim=28 * 28,
@@ -110,8 +113,8 @@ for _ in range(1):
 
             # hidden_dims=[neurons // _ for _ in range(h, 0, -1)],
             hidden_dim_d = hidden_dims_e[::-1]
-            decoder_activations = [nn.Sequential(nn.ReLU(), nn.BatchNorm1d(_)) for _ in hidden_dim_d]
-            decoder_activations.extend([nn.Sequential(nn.ReLU(), nn.BatchNorm1d(28 * 28))])  # dim of output
+            decoder_activations = [nn.Sequential(nn.ReLU()) for _ in hidden_dim_d]
+            decoder_activations.extend([nn.Sequential(nn.ReLU())])  # dim of output
 
             decoder = FCDecoder(
                 latent_dim=l,
@@ -137,15 +140,14 @@ for _ in range(1):
                 train_loss = 0
                 start = time.time()
 
-                for batch_idx, (data, _) in enumerate(train_dataloader):
+                for batch_idx, (data, target) in enumerate(train_dataloader):
                     optimizer_E.zero_grad()
                     optimizer_D.zero_grad()
 
                     encoded_latent = encoder(data)
                     recon = decoder(encoded_latent)
 
-                    wica_loss_ = wica_loss.loss(encoded_latent.data.double(), latent_normalization=True)
-                    loss = criterion(recon, data) + beta * wica_loss_
+                    loss = criterion(recon, data) + beta * MI_loss.loss(recon.detach(), data.detach())
                     loss.backward()
 
                     optimizer_E.step()
@@ -156,37 +158,34 @@ for _ in range(1):
                 end = time.time()
                 loss = train_loss / len(train_dataloader)
 
-                report(epoch, num_epochs, start, end, loss, True)
+                report(epoch, num_epochs, start, end, loss, save_every=2, train=True)
                 save_images_from_epoch(imgs=recon,
                                        epoch=epoch,
                                        folder="../",
                                        save_every=2)
 
                 test_loss = 0
-                wica_loss_t = 0
                 start_t = time.time()
                 encoder.eval()
                 decoder.eval()
 
-                for batch_idx, (data_t, _) in enumerate(test_dataloader):
+                for batch_idx, (data_t, target_t) in enumerate(test_dataloader):
                     encoded_latent = encoder(data_t)
                     recon = decoder(encoded_latent)
 
-                    wica_loss_ = wica_loss.loss(encoded_latent.data.double(), latent_normalization=True)
-                    wica_loss_t += wica_loss_
-                    loss = criterion(recon, data_t) + beta * wica_loss_
+                    loss = criterion(recon, data_t) + beta * MI_loss.loss(recon.detach(), data_t.detach())
 
                     test_loss += loss.data
 
-                end = time.time()
+                end_t = time.time()
 
                 loss_t = test_loss / len(test_dataloader)
-                wica_loss_t_ = wica_loss_t / len(test_dataloader)
 
-                report(epoch, num_epochs, start, end, loss_t, False)
+                report(epoch, num_epochs, start_t, end_t, loss_t, save_every=2, train=False)
                 save_images_from_epoch(imgs=recon,
                                        epoch=epoch,
                                        folder="../",
-                                       save_every=2)
+                                       save_every=2,
+                                       train=False)
 
             print("Training complete")
